dfee32b51f799c56ed2b08d102da724290150da2
Alabe Duarte <alabeduarte@gmail.com>
2019-01-09 15:19:30 +1100

    fix(iterm2): fix iterm2 config location

--- iterm2/set_user_path.sh
+++ iterm2/set_user_path.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -E "s/{{USER_PATH}}/\/Users\/$(whoami)/" iterm2/iterm2-config.plist > \
-  $(whoami)-iterm2-config.plist
+sed -E "s/{{USER_PATH}}/\/Users\/$(whoami)/" iterm2/iterm2-config > \
+  "iterm2/$(whoami)-iterm2-config.plist"
