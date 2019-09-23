cb01d4da5d5fbdf41d8e78e1c4704f9690594443
Alabe Duarte <alabeduarte@gmail.com>
2019-09-23 11:22:54 +1000

    fix(vim-plug): add missing installation script

--- vim/install.sh
+++ vim/install.sh
@@ -13,3 +13,7 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 EOT
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
