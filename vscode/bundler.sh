5ebfc32aedeeeec6b3139e90e6717fda82b88458
Alabe Duarte <alabeduarte@gmail.com>
2019-12-10 09:39:07 +1100

    chore(vscode): remove echo

--- vscode/bundler.sh
+++ vscode/bundler.sh
@@ -6,8 +6,6 @@
 ###
 bundle()
 {
-  echo "yay"
-
   # Creates symbolic link
   rm -rf ~/Library/Application\ Support/Code/User/settings.json || true
   ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
