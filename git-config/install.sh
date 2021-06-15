27b7c6ae56ec1f7e59270186c40aeba70b98a4c4
Alabê Duarte <alabeduarte@gmail.com>
2021-06-15 10:44:12 +1000

    feat(git): Sort git branches by "last activity"
    By @tenderlove (https://twitter.com/tenderlove/status/1392957802163802112?s=21)

--- git-config/install.sh
+++ git-config/install.sh
@@ -14,3 +14,4 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.recent 'branch --sort=-committerdate --format="%(committerdate:relative)%09%(refname:short)"'
