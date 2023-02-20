9867b47de3e4f19688df757a01a6ab8b0038c30c
Alabê Duarte <alabeduarte@gmail.com>
2023-02-20 16:57:08 +1100

    fix(brew): fix installation script

--- homebrew/brew-installer.sh
+++ homebrew/brew-installer.sh
@@ -7,7 +7,7 @@ set -exu
 ###
 install()
 {
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 ###
