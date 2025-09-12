13ab2c20445179c3a675d0c116ce3e68999f5d16
Alabê Duarte <alabeduarte@gmail.com>
2025-09-12 16:02:33 +1000

    refactor(codecompanion): Added `prompt_library` with a "Generate a Commit Message" prompt that formats the staged git diff and user instructions for commit message generation.

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -15,6 +15,38 @@ return {
         },
       },
     },
+    prompt_library = {
+      ["Generate a Commit Message"] = {
+        strategy = "chat",
+        description = "Generate a commit message",
+        opts = {
+          index = 10,
+          is_default = true,
+          is_slash_cmd = true,
+          short_name = "commit",
+          auto_submit = true,
+        },
+        prompts = {
+          {
+            role = "user",
+            content = function()
+              return string.format(
+                [[You are an expert at following the Conventional Commit specification. However, the commit message should not use feat/chore/refactor, I want a single sentence. Also, wrap any function/variable/struct with ``. Given the git diff listed below, please generate a commit message for me:
+
+                  ```diff
+                    %s
+                  ```
+                ]],
+                vim.fn.system("git diff --no-ext-diff --staged")
+              )
+            end,
+            opts = {
+              contains_code = true,
+            },
+          },
+        },
+      },
+    },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
